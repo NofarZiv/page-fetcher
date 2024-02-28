6c429bba144853c35038aa13d3a6931654727df6
NofarZiv <nofarziv13@gmail.com>
2024-02-28 14:48:14 -0330

    created app.

---
+++ fetcher.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+
+request(args[0], (error, response, body) => {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    fs.writeFile(args[1], body, (err) => {
+      if (err) {
+        console.log('error:', err);
+      } else {
+        console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}.`);
+      }
+    });
+  }
+  
+});
